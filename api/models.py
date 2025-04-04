05cdaf951dda11d1217f9f87c4fe31960bda67c0
Luis Rayo <rayo862@hotmail.com>
2025-04-04 15:01:22 -0500

    refac

--- api/models.py
+++ api/models.py
@@ -1,15 +1,20 @@
-from database import get_db_connection
+import psycopg2
 
-def save_beacon_event(mac, latitude, longitude, phone, event_type):
-    conn = get_db_connection()
-    cur = conn.cursor()
+def save_beacon_event(mac, lat, lon, phone, event_type):
+    conn = psycopg2.connect(
+        host="ep-rapid-brook-a4bdlztx-pooler.us-east-1.aws.neon.tech",
+        database="neondb",
+        user="neondb_owner",
+        password="npg_8LBHPVWg0zaK",
+        sslmode="require"
+    )
 
+    cursor = conn.cursor()
     query = """
         INSERT INTO beacon_events (mac_address, latitude, longitude, phone_number, event_type)
         VALUES (%s, %s, %s, %s, %s)
     """
-    cur.execute(query, (mac, latitude, longitude, phone, event_type))
-
+    cursor.execute(query, (mac, lat, lon, phone, event_type))
     conn.commit()
-    cur.close()
+    cursor.close()
     conn.close()
